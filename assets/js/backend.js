70ae182839df9b840457b65e6aaee651f524aaf1
alexunit <alexunit@gmail.com>
2013-02-24 21:47:47 +0300

    backend changes

---
+++ assets/js/backend.js
@@ -0,0 +1,43 @@
+$(function() {
+	// bind tables ordering
+	bindAutoReload();
+	bindCancelButtons();
+});
+
+
+function showSpinner(place) {
+	$(place).parent().append('<div class="spinner">Loading...</div>');
+	$(place).hide();
+}
+function hideSpinner(place) {
+	$(place).siblings('.spinner').hide();
+	$(place).show();
+}
+
+function bindAutoReload() {
+	$('a.orderColumn').on('click',function(evt){
+		var parentRel = $(evt.target).parents('.reloadPart');
+		showSpinner(parentRel);
+		$.ajax({
+			url: evt.target.href,
+			success: function ( data ) {
+				parentRel.html(data);
+				var state = {title: document.title, url: document.location.href};
+				history.pushState(state, state.title, evt.target.href);
+				hideSpinner(parentRel);
+				bindAutoReload();
+			}
+		});
+		return false;
+	});
+}
+
+function bindCancelButtons() {
+	$('input.cancel_btn').on('click',function(evt){
+		var url = $(evt.target).attr('rel');
+		if (url.length>0) {
+			window.location=url;
+		}
+		return false;
+	});
+}
